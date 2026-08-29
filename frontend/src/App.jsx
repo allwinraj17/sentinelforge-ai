81a0917be685e7074c513c485592a5b4f18f9a1e
allwinraj <allwinraj1762k5@gmail.com>
2026-08-29 22:45:27 +0530

    connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-const API_URL = 'http://127.0.0.1:8000'
+const API_URL = 'https://sentinelforge-ai.onrender.com"'
 
 function App() {
   const [file, setFile] = useState(null)
